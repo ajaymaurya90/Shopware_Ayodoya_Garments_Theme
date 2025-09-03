2dca237a1383202eb5785bf073dce7edd3cf8003
ajay shankar maurya <ajaymaurya@ajays-MacBook-Pro.local>
2025-09-03 14:13:27 +0530

    Custom theme development for garments Industry

---
+++ src/AyodoyaGarmentsTheme.php
@@ -0,0 +1,10 @@
+<?php declare(strict_types=1);
+
+namespace AyodoyaGarmentsTheme;
+
+use Shopware\Core\Framework\Plugin;
+use Shopware\Storefront\Framework\ThemeInterface;
+
+class AyodoyaGarmentsTheme extends Plugin implements ThemeInterface
+{
+}
